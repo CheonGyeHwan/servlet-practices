f6f178430b7831826e729ba5325d763cdadd20d5
user <user@DESKTOP-0E1JB75>
2022-01-13 17:29:07 +0900

    emaillist02 #finish

---
+++ emaillist02/src/main/java/com/poscoict/emaillist/dao/test/EmaillistDaoTest.java
@@ -0,0 +1,33 @@
+package com.poscoict.emaillist.dao.test;
+
+import java.util.List;
+import com.poscoict.emaillist.dao.EmaillistDao;
+import com.poscoict.emaillist.vo.EmaillistVo;
+
+public class EmaillistDaoTest {
+
+	public static void main(String[] args) {
+		testInsert();
+		testFindAll();
+	}
+
+	private static void testInsert() {
+		EmaillistVo vo = new EmaillistVo();
+		vo.setFirstName("천");
+		vo.setLastName("계환");
+		vo.setEmail("aabbcc@naver.com");
+		
+		boolean result = new EmaillistDao().insert(vo);
+		System.out.println(result ? "Success" : "Fail");
+	}
+
+	private static void testFindAll() {
+		List<EmaillistVo> list = new EmaillistDao().findAll();
+		for (EmaillistVo vo : list) {
+			System.out.println(vo);
+		}
+	}
+	
+	
+
+}
